d25091e006fbe0c35cd7198b8cf34d54eddc038c
m001108 <m.001108@outlook.com>
2023-08-04 03:30:30 +0900

    #1 project init

---
+++ config-overrides.js
@@ -0,0 +1,38 @@
+const { ProvidePlugin } = require('webpack');
+
+module.exports = function (config, env) {
+    return {
+        ...config,
+        module: {
+            ...config.module,
+            rules: [
+                ...config.module.rules,
+                {
+                    test: /\.m?[jt]sx?$/,
+                    resolve: {
+                        fullySpecified: false,
+                    },
+                },
+            ],
+        },
+        plugins: [
+            ...config.plugins,
+            new ProvidePlugin({
+                process: 'process/browser',
+                Buffer: ["buffer", "Buffer"],
+            }),
+        ],
+        resolve: {
+            ...config.resolve,
+            fallback: {
+                assert: require.resolve('assert'),
+                buffer: require.resolve('buffer'),
+                crypto: require.resolve('crypto-browserify'),
+                stream: require.resolve('stream-browserify'),
+                zlib: require.resolve('browserify-zlib'),
+                path: require.resolve('path-browserify'),
+            },
+        },
+        ignoreWarnings: [/Failed to parse source map/],
+    };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,css,scss}'
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': "0px",
+      // => @media (min-width: 0px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
